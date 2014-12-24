646656eb5968a38efc70f71d520b57fbc05b6248
Viresh <viresh.ramdatmisier@outlook.com>
2014-12-23 17:48:10 -0800

    updates

--- create-vms.ps1
+++ create-vms.ps1
@@ -7,8 +7,17 @@ $vmSize = (Get-AzureRoleSize | where{$_.SupportedByVirtualMachines -eq $true -an
 $location = "West US"
 $cores = 8
 
-$vmCreateMethod = "New-AzureQuickVM"
+#unique identifier for VM name
+$miniGuid = [guid]::NewGuid().ToString().Split("-")[0]
+$vmName = "warpig-" + $miniGuid
+$adminUser = "warpig-admin"
+$password = "Warpig-1@3$"
+$diskLabel = "warpig-disk-" + $miniGuid
+$diskSize = ((Get-AzureRoleSize | where{$_.InstanceSize -eq $vmSize}).VirtualMachineResourceDiskSizeInMb)/1000
 
+$vmCreateMethod = "New-AzureVM"
+
+#get Azure subscription credentials if needed
 #Add-AzureAccount
 
 #pipe Get-AzureSubscription into Select-AzureSubscription
@@ -32,17 +41,41 @@ if((Test-AzureName -Service -Name $cloudServiceName) -eq $false)
   New-AzureService $cloudServiceName -Location $location
 }
 
-#unique identifier for VM name
-$miniGuid = [guid]::NewGuid().ToString().Split("-")[0]
-
 Switch -exact ($vmCreateMethod)
 {
   "New-AzureQuickVM"
   {
-    $adminUser = "warpig-admin"
-    $password = "Warpig-1@3$"
-    $vmName = "warpig-" + $miniGuid
-    $vmCreateCommand = $vmCreateMethod + " " + "-Windows -ServiceName $cloudServiceName -Name $vmName -ImageName $imageName -InstanceSize $vmSize -AdminUsername $adminUser -Password $password"
-    Invoke-Expression $vmCreateCommand
+    $vmCreateMethod = $vmCreateMethod + " -Windows -ServiceName $cloudServiceName -Name $vmName -ImageName $imageName -InstanceSize $vmSize -AdminUsername $adminUser -Password $password"
+    Invoke-Expression $vmCreateMethod
+  }
+  "New-AzureVM"
+  {
+    Write-Output $vmSize
+    $vmConfig = New-AzureVMConfig -Name $vmName -InstanceSize $vmSize -ImageName $imageName
+    $vmConfig | Add-AzureProvisioningConfig -Windows -AdminUsername $adminUser -Password $password
+    $vmConfig | Add-AzureDataDisk -CreateNew -DiskSizeInGB $diskSize -DiskLabel $diskLabel -LUN 0
+    $vmConfig | Add-AzureEndpoint -Name "HTTP" -Protocol tcp -LocalPort 80 -PublicPort 80 -LBSetName "LBHTTP" -DefaultProbe
+    $vmConfig | Add-AzureEndpoint -Name "HTTPS" -Protocol tcp -LocalPort 443 -PublicPort 443 -LBSetName "LBHTTPS" -DefaultProbe
+    
+    New-AzureVM -ServiceName $cloudServiceName -VMs $vmConfig
+
+    $vmConfig = Get-AzureVM -ServiceName $cloudServiceName -Name $vmName
+    if($vmConfig | Get-AzureEndpoint | where Name -eq "RemoteDesktop")
+    {
+      $vmConfig | Remove-AzureEndpoint -Name "RemoteDesktop"
+      $vmConfig | Update-AzureVM
+    }
+
+    $vmConfig = Get-AzureVM -ServiceName $cloudServiceName -Name $vmName
+    if(!($vmConfig | Get-AzureEndpoint | where Name -eq "RemoteDesktop"))
+    {
+      $vmConfig | Add-AzureEndpoint -Name "RemoteDesktop" -Protocol tcp -LocalPort 3389
+      $vmConfig | Update-AzureVM
+      Get-AzureRemoteDesktopFile -ServiceName $cloudServiceName -Name $vmName -Launch
+    }
   }
 }
+
+Get-AzureVM -ServiceName $cloudServiceName | Stop-AzureVM -Force
+Remove-AzureService -ServiceName $cloudServiceName  -Force
+
